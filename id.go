d8e458a75e0e0609819b02821690525fc8d51f03
Naser Mirzaei <nasermirzaei89@gmail.com>
2020-06-22 22:10:12 +0430

    use once instead of init

--- id.go
+++ id.go
@@ -4,27 +4,30 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 )
 
-var id = make(chan string)
-
-func init() {
-	go func() {
-		h := sha1.New()
-		c := []byte(time.Now().String())
-		for {
-			_, err := h.Write(c)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			id <- fmt.Sprintf("%x", h.Sum(nil))
-		}
-	}()
-}
+var (
+	id   = make(chan string)
+	once sync.Once
+)
 
 // New returns a new random string
 func New() string {
+	once.Do(func() {
+		go func() {
+			h := sha1.New()
+			c := []byte(time.Now().String())
+			for {
+				_, err := h.Write(c)
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				id <- fmt.Sprintf("%x", h.Sum(nil))
+			}
+		}()
+	})
 	return <-id
 }
--- id_test.go
+++ id_test.go
@@ -8,23 +8,12 @@ import (
 func TestNew(t *testing.T) {
 	id := New()
 	if l := len(id); l != 40 {
-		t.Errorf("generated strign length is '%d' not '40'", l)
+		t.Errorf("generated string length is '%d' not '40'", l)
 	}
 }
 
 func BenchmarkNew(b *testing.B) {
-	ids := make([]string, 0)
-	for {
-		l := len(ids)
-		ids = append(ids, New())
-		for i := range ids {
-			if i < l && ids[i] == ids[l] {
-				b.Errorf("generated duplicate id in round %d and %d", i, l)
-			}
-		}
-
-		if len(ids) > b.N {
-			break
-		}
+	for i := 0; i < b.N; i++ {
+		_ = New()
 	}
 }
